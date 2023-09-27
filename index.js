1b5e413591da67cbc04b202c8c8e9bdcf08d2461
JulianGrafen <juliangraefen@gmail.com>
2023-09-27 20:02:30 +0200

    working on transition

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var timesClicked = 0;
 
-function doubleClick(){
-
-    var element = document.getElementById("socialview");
-
+function doubleClick(element){
     timesClicked++;
     if (timesClicked % 2 === 0){
         element.style.width = "100px";
         element.style.height = "auto";
+        document.getElementById("aboutMe1").style.marginTop ="45rem";
+        document.getElementById("aboutMe2").style.marginTop ="45rem";
+        document.getElementById("personalContainer").style.marginTop ="35rem";
+
     } else {
-        element.style.width = "60rem";
-        element.style.height = "10rem";
+        element.style.width = "80rem";
+        element.style.height = "90rem";
         document.getElementById("aboutMe1").style.marginTop ="60rem";
         document.getElementById("aboutMe2").style.marginTop ="60rem";
-        element.toggleClass("transform-active")
+        document.getElementById("project").style.marginTop ="00rem";
+        document.getElementById("project").style.paddingTop ="0rem";
+
 
     }
 
