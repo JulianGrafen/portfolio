678130c8f0b0c1ecf042442117b65e58859d7ab8
JulianGrafen <juliangraefen@gmail.com>
2023-10-27 11:29:51 +0200

    Added Toolbox -> WIP

--- index.js
+++ index.js
@@ -2,23 +2,40 @@ var timesClicked = 0;
 
 function doubleClick(element){
     timesClicked++;
+    console.log(timesClicked)
     if (timesClicked % 2 === 0){
-        element.style.width = "100px";
-        element.style.height = "auto";
+        
+        //About me Layers
         document.getElementById("aboutMe1").style.marginTop ="45rem";
         document.getElementById("aboutMe2").style.marginTop ="45rem";
-        document.getElementById("personalContainer").style.marginTop ="35rem";
+        document.getElementById("personalContainer").style.marginTop ="39rem";
+        document.getElementById("myTools").style.marginTop ="60rem";
+
+
+
+
 
     } else {
-        element.style.width = "80rem";
-        element.style.height = "90rem";
+    
+        //About me Layers
         document.getElementById("aboutMe1").style.marginTop ="60rem";
         document.getElementById("aboutMe2").style.marginTop ="60rem";
-        document.getElementById("project").style.marginTop ="00rem";
-        document.getElementById("project").style.paddingTop ="0rem";
+        document.getElementById("personalContainer").style.marginTop ="50rem";
+        document.getElementById("myTools").style.marginTop ="70rem";
+
+
+
+        
+
+
+
+        
+
+
 
 
     }
+
 
 }
 
